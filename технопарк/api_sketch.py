0875d7cb8e160728b1b5834e385dac2b86ab19fc
Astakhov Sergey <fzastahov@gmail.com>
2021-11-02 12:54:01 +0300

    api create segment

--- технопарк/api_sketch.py
+++ технопарк/api_sketch.py
@@ -1,9 +1,10 @@
 import requests
 from urllib.parse import urljoin
+import json
 
 id = 1401571
 csrf_token = "ut3qTnrRuyuaPXIsosd2EGMntCxRj9tP9CRbA7IXOtnZrgkHDvaUMnzmZyKsbEN8"
-cookie = "tmr_lvid=97eb38a6b9b3da5caa5eb1138bd057aa; tmr_lvidTS=1635089481459; _ga=GA1.3.760277518.1635089482; z=xzml6o34u0l1xxydaq430gdg9lkskfcs; _ga=GA1.2.760277518.1635089482; mrcu=327F61757C5331593AF21C11C851; p=ZAAAAEnzFAAA; s=rt=1|dpr=1.25; __utma=144340137.760277518.1635089482.1635089493.1635089493.1; __utmz=144340137.1635089493.1.1.utmcsr=target.my.com|utmccn=(referral)|utmcmd=referral|utmcct=/; _gid=GA1.3.735643551.1635841274; _gid=GA1.2.735643551.1635841274; _gac_UA-54874995-1=1.1635841276.EAIaIQobChMIq4Coz5_58wIVD2YYCh1i1g13EAAYASAAEgKEhvD_BwE; mc=a79c9b0ab4ad7dae9f16238df3403402b265d13134383632; sdc=FUE7lcj4tRKsGOxY; _gac_UA-54874995-1=1.1635841276.EAIaIQobChMIq4Coz5_58wIVD2YYCh1i1g13EAAYASAAEgKEhvD_BwE; _gcl_au=1.1.98288910.1635841303; _fbp=fb.1.1635841303613.1674862734; csrftoken=ut3qTnrRuyuaPXIsosd2EGMntCxRj9tP9CRbA7IXOtnZrgkHDvaUMnzmZyKsbEN8; tmr_detect=0%7C1635841477183; tmr_reqNum=27"
+cookie = "tmr_lvid=97eb38a6b9b3da5caa5eb1138bd057aa; tmr_lvidTS=1635089481459; _ga=GA1.3.760277518.1635089482; z=xzml6o34u0l1xxydaq430gdg9lkskfcs; _ga=GA1.2.760277518.1635089482; mrcu=327F61757C5331593AF21C11C851; p=ZAAAAEnzFAAA; s=rt=1|dpr=1.25; __utma=144340137.760277518.1635089482.1635089493.1635089493.1; __utmz=144340137.1635089493.1.1.utmcsr=target.my.com|utmccn=(referral)|utmcmd=referral|utmcct=/; _gid=GA1.3.735643551.1635841274; _gid=GA1.2.735643551.1635841274; _gac_UA-54874995-1=1.1635841276.EAIaIQobChMIq4Coz5_58wIVD2YYCh1i1g13EAAYASAAEgKEhvD_BwE; mc=a79c9b0ab4ad7dae9f16238df3403402b265d13134383632; sdc=FUE7lcj4tRKsGOxY; _gac_UA-54874995-1=1.1635841276.EAIaIQobChMIq4Coz5_58wIVD2YYCh1i1g13EAAYASAAEgKEhvD_BwE; _gcl_au=1.1.98288910.1635841303; _fbp=fb.1.1635841303613.1674862734; csrftoken=ut3qTnrRuyuaPXIsosd2EGMntCxRj9tP9CRbA7IXOtnZrgkHDvaUMnzmZyKsbEN8; tmr_detect=0%7C1635841477183; tmr_reqNum=44"
 
 
 class ApiClient:
@@ -14,8 +15,9 @@ class ApiClient:
 
     def delete_segment(self, segment_id):
         # url = urljoin(
-            # "https://target.my.com/api/v2/remarketing/segments", (str(segment_id) + ".json"))
-        url = "https://target.my.com/api/v2/remarketing/segments/" + str(segment_id) + ".json"
+        # "https://target.my.com/api/v2/remarketing/segments", (str(segment_id) + ".json"))
+        url = "https://target.my.com/api/v2/remarketing/segments/" + \
+            str(segment_id) + ".json"
         headers = {
             'X-CSRFToken': self.csrf_token,
             'Cookie': self.cookie
@@ -23,8 +25,36 @@ class ApiClient:
         response = self.session.request("DELETE", url, headers=headers)
         return response
 
+    def create_segment(self, name):
+        params = "?fields=relations__object_type,relations__object_id,relations__params,relations__params__score,relations__id,relations_count,id,name,pass_condition,created,campaign_ids,users,flags"
+        url = "https://target.my.com/api/v2/remarketing/segments.json" + params
+        headers = {
+            'X-CSRFToken': self.csrf_token,
+            'Cookie': self.cookie,
+            'Content-Type': 'application/json'
+        }
+        payload = {
+            "name": name,
+            "pass_condition": 1,
+            "relations": [
+                {
+                    "object_type": "remarketing_player",
+                    "params": {
+                        "type": "positive",
+                        "left": 365,
+                        "right": 0
+                    }
+                }
+            ],
+            "logicType": "or"
+        }
+        payload = json.dumps(payload)
+        # print(payload)
+        response = self.session.request("POST", url, headers=headers, data=payload)
+        return response
 
-if __name__ == "__main__":
+
+def test1():
     client = ApiClient()
     client.csrf_token = csrf_token
     client.cookie = cookie
@@ -33,3 +63,20 @@ if __name__ == "__main__":
     print(resp.headers)
     print(dir(resp))
     assert resp.status_code == 204
+
+
+def test2():
+    client = ApiClient()
+    client.csrf_token = csrf_token
+    client.cookie = cookie
+    resp = client.create_segment("room 50541e3")
+    print(resp)
+    print(resp.json())
+    # print(resp.headers)
+    # print(dir(resp))
+    assert resp.status_code == 200
+
+
+
+if __name__ == "__main__":
+    test2()
