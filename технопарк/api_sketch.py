430544ed53cc27795204b9ae5258aaf9406a747b
Astakhov Sergey <fzastahov@gmail.com>
2021-11-02 13:53:03 +0300

    TP - delete campaign

--- технопарк/api_sketch.py
+++ технопарк/api_sketch.py
@@ -7,6 +7,7 @@ csrf_token = "ut3qTnrRuyuaPXIsosd2EGMntCxRj9tP9CRbA7IXOtnZrgkHDvaUMnzmZyKsbEN8"
 # cookie = "tmr_lvid=97eb38a6b9b3da5caa5eb1138bd057aa; tmr_lvidTS=1635089481459; _ga=GA1.3.760277518.1635089482; z=xzml6o34u0l1xxydaq430gdg9lkskfcs; _ga=GA1.2.760277518.1635089482; mrcu=327F61757C5331593AF21C11C851; p=ZAAAAEnzFAAA; s=rt=1|dpr=1.25; __utma=144340137.760277518.1635089482.1635089493.1635089493.1; __utmz=144340137.1635089493.1.1.utmcsr=target.my.com|utmccn=(referral)|utmcmd=referral|utmcct=/; _gid=GA1.3.735643551.1635841274; _gid=GA1.2.735643551.1635841274; _gac_UA-54874995-1=1.1635841276.EAIaIQobChMIq4Coz5_58wIVD2YYCh1i1g13EAAYASAAEgKEhvD_BwE; mc=a79c9b0ab4ad7dae9f16238df3403402b265d13134383632; sdc=FUE7lcj4tRKsGOxY; _gac_UA-54874995-1=1.1635841276.EAIaIQobChMIq4Coz5_58wIVD2YYCh1i1g13EAAYASAAEgKEhvD_BwE; _gcl_au=1.1.98288910.1635841303; _fbp=fb.1.1635841303613.1674862734; csrftoken=ut3qTnrRuyuaPXIsosd2EGMntCxRj9tP9CRbA7IXOtnZrgkHDvaUMnzmZyKsbEN8; tmr_detect=0%7C1635841477183; tmr_reqNum=44"
 cookie = "tmr_lvid=97eb38a6b9b3da5caa5eb1138bd057aa; tmr_lvidTS=1635089481459; _ga=GA1.3.760277518.1635089482; z=xzml6o34u0l1xxydaq430gdg9lkskfcs; _ga=GA1.2.760277518.1635089482; mrcu=327F61757C5331593AF21C11C851; p=ZAAAAEnzFAAA; s=rt=1|dpr=1.25; __utma=144340137.760277518.1635089482.1635089493.1635089493.1; __utmz=144340137.1635089493.1.1.utmcsr=target.my.com|utmccn=(referral)|utmcmd=referral|utmcct=/; _gid=GA1.3.735643551.1635841274; _gid=GA1.2.735643551.1635841274; _gac_UA-54874995-1=1.1635841276.EAIaIQobChMIq4Coz5_58wIVD2YYCh1i1g13EAAYASAAEgKEhvD_BwE; mc=a79c9b0ab4ad7dae9f16238df3403402b265d13134383632; sdc=FUE7lcj4tRKsGOxY; _gac_UA-54874995-1=1.1635841276.EAIaIQobChMIq4Coz5_58wIVD2YYCh1i1g13EAAYASAAEgKEhvD_BwE; _gcl_au=1.1.98288910.1635841303; _fbp=fb.1.1635841303613.1674862734; csrftoken=ut3qTnrRuyuaPXIsosd2EGMntCxRj9tP9CRbA7IXOtnZrgkHDvaUMnzmZyKsbEN8; tmr_detect=0%7C1635846825207; tmr_reqNum=58; _gat_UA-54874995-1=1"
 
+
 class ApiClient:
 
     csrf_token = None
@@ -50,7 +51,8 @@ class ApiClient:
         }
         payload = json.dumps(payload)
         # print(payload)
-        response = self.session.request("POST", url, headers=headers, data=payload)
+        response = self.session.request(
+            "POST", url, headers=headers, data=payload)
         return response
 
     # def get_segments(self):
@@ -88,7 +90,24 @@ class ApiClient:
                 return True
         return False
 
-
+    # 204 code
+    def delete_campaign(self, id):
+        url = "https://target.my.com/api/v2/campaigns/mass_action.json"
+        headers = {
+            'X-CSRFToken': self.csrf_token,
+            'Cookie': self.cookie,
+            'Content-Type': 'application/json'
+        }
+        payload = [
+            {
+                "id": id,
+                "status": "deleted"
+            }
+        ]
+        payload = json.dumps(payload)
+        response = self.session.request(
+            "POST", url, headers=headers, data=payload)
+        return response
 
 
 def test1():
@@ -113,6 +132,7 @@ def test2():
     # print(dir(resp))
     assert resp.status_code == 200
 
+
 def test3():
     client = ApiClient()
     client.csrf_token = csrf_token
@@ -124,5 +144,15 @@ def test3():
     # print(resp.text.count("room 50541e3"))
     assert(client.check_segment_presence("room 50541e3"))
 
+def test4():
+    client = ApiClient()
+    client.csrf_token = csrf_token
+    client.cookie = cookie
+    resp = client.delete_campaign(46498588)
+    # print(resp.json())
+    assert resp.status_code == 204
+
+
+
 if __name__ == "__main__":
-    test3()
+    test4()
